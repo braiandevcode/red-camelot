ee725f32978b5f9862a46735d593b880f2a9d5a7
Braian Palacios <braianpalacios699@gmail.com>
2024-04-14 20:43:15 -0300

    probando cambio de configuración en vite

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host: '0.0.0.0',
-    port:8080
-  },
-  envDir: '.',
+  // server:{
+  //   host: '0.0.0.0',
+  //   port:8080
+  // },
+  // envDir: '.',
   // base: '/red-camelot/',
   base: 'https://braiandevcode.github.io/red-camelot/'
 })
